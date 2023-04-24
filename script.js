071f89f9d114242a1ea9aa619df8d2c9419b34d5
proggy1 <ahmedfcbmns5@gmail.com>
2023-04-24 14:34:56 +0200

    Added Final Html And Javascript

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-fetch("https://api.quotable.io/random")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+const quoteElement = document.querySelector("main .quote");
+const authorElement = document.querySelector("main .author");
+const anotherQuoteButton = document.querySelector("main button");
+
+const fetchQuoteApi = async () => {
+  const response = await fetch("https://api.quotable.io/random");
+  const data = await response.json();
+  return data;
+};
+
+const populateQuote = async () => {
+  const data = await fetchQuoteApi();
+  const quote = data.content;
+  const author = data.author;
+
+  quoteElement.textContent = quote;
+  authorElement.textContent = `-${author}`;
+};
+
+anotherQuoteButton.addEventListener("click", populateQuote);
+
+populateQuote();
